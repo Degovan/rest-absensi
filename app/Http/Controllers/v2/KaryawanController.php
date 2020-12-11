f591539333146096a9bd8c6ae77d91375b8991e0
To My <myfragithub@gmail.com>
2020-12-11 13:14:16 +0700

    updating rest data karyawan

--- app/Http/Controllers/v2/KaryawanController.php
+++ app/Http/Controllers/v2/KaryawanController.php
@@ -4,31 +4,30 @@ namespace App\Http\Controllers\v2;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Karyawan;
+use App\User;
 
 class KaryawanController extends Controller
 {
     public function index(Request $request)
     {
-
         $api_token = request('api_token');
-        $karyawan = Karyawan::where('api_token', $api_token)->get()[0];
+        $karyawan = User::with(['school', 'position', 'schedule', 'jadwal', 'cabang', 'status'])->where('api_token', $api_token)->get();
 
-        if ($karyawan === 0) {
+        if (count($karyawan) == 0) {
             return response()->json([
-                "code"      => 400,
-                "success"   => false,
-                "message"   => "data tidak ditemukan, anda harus login terlebih dahulu",
+                'code'      => 400,
+                'success'   => (boolean) false,
+                'message'   => 'error, employee not found',
             ], 400);
-        }else {
-            return response()->json([
-                "code"      => 200,
-                "success"   => true,
-                "message"   => "success",
-                "data"      =>  [
-                                'employees' => $karyawan
-                            ]
-            ], 200);
         }
+
+        return response()->json([
+            'code'      => 200,
+            'success'   => (boolean) true,
+            'message'   => 'successfully, employee data found',
+            'data'      =>  [
+                            'employee' => $karyawan
+                        ]
+        ], 200);
     }
 }
